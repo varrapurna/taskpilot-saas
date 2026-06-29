33999f3839407220152b08717a9d13ec474ad60e
mh-purna <varrapurnachandrareddy@mhcognition.in>
2026-06-29 22:50:20 +0530

    Proxy MH Leads embed through Next.js to bypass missing CORS header

--- app/form-test/page.js
+++ app/form-test/page.js
@@ -6,7 +6,7 @@ export default function FormTest() {
       <Script id="mhl-config" strategy="beforeInteractive">
         {`
           window.mhl_id = "d23a8709-6409-43b0-8a98-127e06ec6d47";
-          window.mhl_ingest_base = "https://mhleadsg.exploremira.com/api/ingestion";
+          window.mhl_ingest_base = "/api/mhl";
         `}
       </Script>
       <Script
