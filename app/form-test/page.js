44dab8f8261cce18089f89ba05adf10ed3d1ab95
mh-purna <varrapurnachandrareddy@mhcognition.in>
2026-06-29 22:26:12 +0530

    Add MH Leads form test page (/form-test)

---
+++ app/form-test/page.js
@@ -0,0 +1,19 @@
+import Script from 'next/script';
+
+export default function FormTest() {
+  return (
+    <>
+      <Script id="mhl-config" strategy="beforeInteractive">
+        {`
+          window.mhl_id = "d23a8709-6409-43b0-8a98-127e06ec6d47";
+          window.mhl_ingest_base = "https://mhleadsg.exploremira.com/api/ingestion";
+        `}
+      </Script>
+      <Script
+        src="https://d1h7xhvkv25xoi.cloudfront.net/form.render.js"
+        strategy="afterInteractive"
+      />
+      <div id="mhl-form"></div>
+    </>
+  );
+}
